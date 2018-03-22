fc39e41e1640075376f21a7a55ab6dc4ca429879
LaiYizhou <lai_yizhou@163.com>
2018-03-22 15:13:54 +0800

    Init
    Init

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Unity-ScrollViewNevigation
-Unity3D中使用ScrollView时，实现滑动定位到指定位置
+###1. 介绍
+
+在Unity3D中，使用ScrollView时，
+
+经常需要让游戏在运行时，将ScrollView定位到某一指定Item上。
+
+比如：进入游戏后，在关卡界面大地图推图指当前关卡等。
+
+**脚本`ScrollViewNevigation.cs`  则可以实现此功能**。
+
+
+
+### 2. 说明
+
+- 请尽量保证ScrollView组件**层级及命名**如下：
+
+![018032215043](C:\Users\Administrator\Documents\GitHub\Unity-ScrollViewNevigation\Images\20180322150434.png)
+
+​	因为为了减少使用者拖拽赋值工作量，在代码中，已经根据层级和命名初始化完毕
+
+
+
+- 将脚本`ScrollViewNevigation.cs` 挂在 ScrollView 上
+
+![018032215074](C:\Users\Administrator\Documents\GitHub\Unity-ScrollViewNevigation\Images\20180322150747.png)
+
+​	
+
+- 调用其中方法`Nevigate()`即可
+
+  例如：
+
+  ```c#
+  Nevigate(content.GetChild(45).GetComponent<RectTransform>());
+  ```
+
+  ​
+
+- 在脚本中，为了实现平滑移动而定位的效果，使用了DoTween插件
---
+++ ScrollViewNevigation.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class ScrollViewNevigation : MonoBehaviour
+{
+
+    private ScrollRect scrollRect;
+    private RectTransform viewport;
+    private RectTransform content;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	    Init();
+	    //Nevigate(content.GetChild(45).GetComponent<RectTransform>());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void Init()
+    {
+        if (scrollRect == null)
+        {
+            scrollRect = this.GetComponent<ScrollRect>();
+        }
+
+        if (viewport == null)
+        {
+            viewport = this.transform.Find("Viewport").GetComponent<RectTransform>();
+        }
+
+        if (content == null)
+        {
+            content = this.transform.Find("Viewport/Content").GetComponent<RectTransform>();
+        }
+    }
+
+    public void Nevigate(RectTransform item)
+    {
+
+        Vector3 itemCurrentLocalPostion = scrollRect.GetComponent<RectTransform>().InverseTransformVector(ConvertLocalPosToWorldPos(item));
+        Vector3 itemTargetLocalPos = scrollRect.GetComponent<RectTransform>().InverseTransformVector(ConvertLocalPosToWorldPos(viewport));
+
+        Vector3 diff = itemTargetLocalPos - itemCurrentLocalPostion;
+        diff.z = 0.0f;
+
+        var newNormalizedPosition = new Vector2(
+            diff.x / (content.GetComponent<RectTransform>().rect.width - viewport.rect.width),
+            diff.y / (content.GetComponent<RectTransform>().rect.height - viewport.rect.height)
+            );
+
+        newNormalizedPosition = scrollRect.GetComponent<ScrollRect>().normalizedPosition - newNormalizedPosition;
+
+        newNormalizedPosition.x = Mathf.Clamp01(newNormalizedPosition.x);
+        newNormalizedPosition.y = Mathf.Clamp01(newNormalizedPosition.y);
+
+        DOTween.To(() => scrollRect.GetComponent<ScrollRect>().normalizedPosition, x => scrollRect.GetComponent<ScrollRect>().normalizedPosition = x, newNormalizedPosition, 0.8f);
+    }
+
+    private Vector3 ConvertLocalPosToWorldPos(RectTransform target)
+    {
+        var pivotOffset = new Vector3(
+            (0.5f - target.pivot.x) * target.rect.size.x,
+            (0.5f - target.pivot.y) * target.rect.size.y,
+            0f);
+
+        var localPosition = target.localPosition + pivotOffset;
+
+        return target.parent.TransformPoint(localPosition);
+    }
+}
